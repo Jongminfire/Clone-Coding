734584402f19c8a18154b148f509418bd7b8388c
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-08-04 23:32:51 +0900

    초기에 메인페이지 컨텐츠 나오도록 fetchPage 실행

--- Socio design/fetchPage.js
+++ Socio design/fetchPage.js
@@ -1,16 +1,16 @@
 function fetchPage(name){
-    fetch(name).then(function(response){
-      response.text().then(function(text){
-        window.scrollTo({top:location, behavior:'smooth'});         // 스크롤 맨 위로 올리는 함수
+  fetch(name).then(function(response){
+    response.text().then(function(text){
+      window.scrollTo({top:location, behavior:'smooth'});         // 스크롤 맨 위로 올리는 함수
+      
+      $('.content').fadeOut(1000);        // fadeout 함수
 
-        $('.content').fadeOut(1000);        // fadeout 함수
+      setTimeout(function(){              // delay 함수
+      document.querySelector('.content').innerHTML = text;
+      $('.content').fadeIn(300);          // fadein 함수
+      },1000);
+  });
+})
+}
 
-        setTimeout(function(){              // delay 함수
-        document.querySelector('.content').innerHTML = text;
-        $('.content').fadeIn(300);          // fadein 함수
-        },1000);
-    });
-  })
-  }
-
-  fetchPage("main_content.html");           // 초기 컨텐츠
+fetchPage("main_content.html");           // 초기 컨텐츠
