fdb1338808540f455ca05f66079c8f1da5e35cac
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-08-10 22:51:07 +0900

    슬라이드 페이드 인 효과 및 중첩 제거
    1. 슬라이드에 페이드 인 효과 삽입
    2. 기존에 페이지 넘김 함수가 중첩되던 것 setInterval 과 clearInterval로 해결

--- Socio design/slider.js
+++ Socio design/slider.js
@@ -1,4 +1,6 @@
 var index=4;                        // showSlide에서 바로 값이 바뀌므로
+var pre=3;
+var timer=setInterval(showSlide,4000);
 
 function showSlide(){
     var i=0;
@@ -12,16 +14,43 @@ function showSlide(){
     }
 
     index++;
+    pre=index-1;
 
     if(index===slide.length)
     {
         index=0;
+        pre=slide.length-1;
     }
 
     slide[index].style.display="block";
     dot[index].style.backgroundColor="black";
 
-    setTimeout(showSlide,4000); // 함수 뒤에 괄호 붙이면 안됨
+    crossfade(pre,index);
+
+    //setTimeout(showSlide,4000); // 함수 뒤에 괄호 붙이면 안됨
+}
+
+function crossfade(pre,index){
+    var slide = document.getElementsByClassName('slider-content')
+    var op=0.1;
+
+    slide[pre].style.display="block";
+    slide[index].style.zIndex=1;
+
+    var timer = setInterval(()=>{
+        if(op>=1.0)
+        {
+            clearInterval(timer);
+            slide[pre].style.opacity=0;
+            slide[index].style.zIndex=0;
+            slide[pre].style.display="none";
+        }
+
+        slide[index].style.opacity=op;
+        slide[index].style.filter = 'alpha(opacity=' + op * 100 + ")";
+
+        op+=op*0.05;
+    },10);
 }
 
 function changeSlide(num)
@@ -35,8 +64,18 @@ function changeSlide(num)
         dot[i].style.backgroundColor="white";
     }
 
+    pre=index;
     index=num;
+
+    crossfade(pre,index);
 
     slide[index].style.display="block";
     dot[index].style.backgroundColor="black";
+
+    stopSlide();
+    timer=setInterval(showSlide,4000);
+}
+
+function stopSlide(){
+    clearInterval(timer);
 }
